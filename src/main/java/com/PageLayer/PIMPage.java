a6b012693b5f227a1fc9cf6ce6922e1a880bb581
praf002 <74486575+praf002@users.noreply.github.com>
2021-05-26 20:29:32 +0530

    Update PIMPage.java

--- src/main/java/com/PageLayer/PIMPage.java
+++ src/main/java/com/PageLayer/PIMPage.java
@@ -6,4 +6,9 @@ public class PIMPage {
 	public static void main(String[] args) {
 		System.out.println("PIM Page layer.....");
 	}
+	
+	public static void m1(){
+	
+		System.out.println("it is static method added using git hub...")
+	}
 }
