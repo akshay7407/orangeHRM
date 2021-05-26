e92cc57f877f81fdafad027ec41b7e6f68d1d600
Praful Pawar <praf002@gmail.com>
2021-05-26 20:09:23 +0530

    we have added PIM Page functinality Test cases.

---
+++ src/test/java/TestLayer/PIMPageTest.java
@@ -0,0 +1,16 @@
+package TestLayer;
+
+import org.testng.annotations.Test;
+
+import com.BaseLayer.BaseClass;
+
+public class PIMPageTest  extends BaseClass{
+	
+	
+	@Test
+	public void stepUp() {
+	
+		System.out.println("PIM Page Test Layer code..");
+		
+	}
+}
---
+++ src/test/java/TestLayer/UserPageTest.java
@@ -0,0 +1,8 @@
+package TestLayer;
+
+public class UserPageTest {
+
+	public static void main(String[] args) {
+		System.out.println("main  method");
+	}
+}
